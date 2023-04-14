46710af9e604a61f538d1216f62a1b7c312f7f58
AdrianUm <126091005+AdrianUm@users.noreply.github.com>
2023-04-14 14:01:20 +0200

    proyecto

--- goldendumbbell/src/main/java/es/uma/ingsoftware/goldendumbbell/repository/Productorepository.java
+++
@@ -1,8 +0,0 @@
-package es.uma.ingsoftware.goldendumbbell.repository;
-
-import es.uma.ingsoftware.goldendumbbell.model.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Productorepository extends JpaRepository<Producto,Integer> {
-
-}
