fa5227ecccea1049a26e6c1adacc3a43352df83c
rociogzm22 <125653371+rociogzm22@users.noreply.github.com>
2023-05-03 21:13:01 +0200

    dieta finalizada

--- oficial/src/main/java/es/uma/ingsoftware/goldendumbbell/Controller/AlimentoController.java
+++ oficial/src/main/java/es/uma/ingsoftware/goldendumbbell/Controller/AlimentoController.java
@@ -31,50 +31,229 @@ public class AlimentoController {
     }
 
     @RequestMapping("/dieta/dietauno")
-    public String listaRutinatres(Model model){
+    public String Dietauno(Model model){
         List<Alimento> alimentos = alimentoService.getAll();
         List<Alimento> desayuno= new ArrayList<>();
         List<Alimento> almuerzo= new ArrayList<>();
         List<Alimento> snack= new ArrayList<>();
         List<Alimento> cena= new ArrayList<>();
         int i= 0;
-        for (Alimento p : alimentos) {
-            for(Alimento a : alimentos){
-                if(a.getN_dieta() == 1 && a.getComida().equalsIgnoreCase("Almuerzo")){
-                    almuerzo.add(i,a);
-                }
-            }
-            model.addAttribute("almuerzouno", almuerzo);
-        }
-        i= 0;
-        for (Alimento p : alimentos) {
-            for(Alimento a : alimentos){
-                if(a.getN_dieta() == 1 && a.getComida().equalsIgnoreCase("Desayuno")){
-                    desayuno.add(i,a);
-                }
+        for (Alimento a : alimentos) {
+            if(a.getN_dieta() == 1 && a.getComida().equalsIgnoreCase("Desayuno")){
+                desayuno.add(i,a);
+                i++;
             }
             model.addAttribute("desayunouno", desayuno);
         }
+
         i= 0;
-        for (Alimento p : alimentos) {
-            for(Alimento a : alimentos){
-                if(a.getN_dieta() == 1 && a.getComida().equalsIgnoreCase("Snack")){
-                    snack.add(i,a);
-                }
+        for (Alimento a : alimentos) {
+            if(a.getN_dieta() == 1 && a.getComida().equalsIgnoreCase("Almuerzo")){
+                almuerzo.add(i,a);
+                i++;
+            }
+            model.addAttribute("almuerzouno", almuerzo);
+        }
+
+        i= 0;
+        for (Alimento a : alimentos) {
+            if(a.getN_dieta() == 1 && a.getComida().equalsIgnoreCase("Snack")){
+                snack.add(i,a);
+                i++;
             }
             model.addAttribute("snackuno", snack);
         }
+
         i= 0;
-        for (Alimento p : alimentos) {
-            for(Alimento a : alimentos){
-                if(a.getN_dieta() == 1 && a.getComida().equalsIgnoreCase("Cena")){
-                    cena.add(i,a);
-                }
+        for (Alimento a : alimentos) {
+            if(a.getN_dieta() == 1 && a.getComida().equalsIgnoreCase("Cena")){
+                cena.add(i,a);
+                i++;
             }
             model.addAttribute("cenauno", cena);
         }
-
         return "dieta/dietauno";
+    }
+
+
+    @RequestMapping("/dieta/dietados")
+    public String Dietados(Model model){
+        List<Alimento> alimentos = alimentoService.getAll();
+        List<Alimento> desayuno= new ArrayList<>();
+        List<Alimento> almuerzo= new ArrayList<>();
+        List<Alimento> snack= new ArrayList<>();
+        List<Alimento> cena= new ArrayList<>();
+        int i= 0;
+        for (Alimento a : alimentos) {
+            if(a.getN_dieta() == 2 && a.getComida().equalsIgnoreCase("Desayuno")){
+                desayuno.add(i,a);
+                i++;
+            }
+            model.addAttribute("desayunodos", desayuno);
+        }
+
+        i= 0;
+        for (Alimento a : alimentos) {
+            if(a.getN_dieta() == 2 && a.getComida().equalsIgnoreCase("Almuerzo")){
+                almuerzo.add(i,a);
+                i++;
+            }
+            model.addAttribute("almuerzodos", almuerzo);
+        }
+
+        i= 0;
+        for (Alimento a : alimentos) {
+            if(a.getN_dieta() == 2 && a.getComida().equalsIgnoreCase("Snack")){
+                snack.add(i,a);
+                i++;
+            }
+            model.addAttribute("snackdos", snack);
+        }
+
+        i= 0;
+        for (Alimento a : alimentos) {
+            if(a.getN_dieta() == 2 && a.getComida().equalsIgnoreCase("Cena")){
+                cena.add(i,a);
+                i++;
+            }
+            model.addAttribute("cenados", cena);
+        }
+        return "dieta/dietados";
+    }
+
+    @RequestMapping("/dieta/dietatres")
+    public String Dietatres(Model model){
+        List<Alimento> alimentos = alimentoService.getAll();
+        List<Alimento> desayuno= new ArrayList<>();
+        List<Alimento> almuerzo= new ArrayList<>();
+        List<Alimento> snack= new ArrayList<>();
+        List<Alimento> cena= new ArrayList<>();
+        int i= 0;
+        for (Alimento a : alimentos) {
+            if(a.getN_dieta() == 3 && a.getComida().equalsIgnoreCase("Desayuno")){
+                desayuno.add(i,a);
+                i++;
+            }
+            model.addAttribute("desayunotres", desayuno);
+        }
+
+        i= 0;
+        for (Alimento a : alimentos) {
+            if(a.getN_dieta() == 3 && a.getComida().equalsIgnoreCase("Almuerzo")){
+                almuerzo.add(i,a);
+                i++;
+            }
+            model.addAttribute("almuerzotres", almuerzo);
+        }
+
+        i= 0;
+        for (Alimento a : alimentos) {
+            if(a.getN_dieta() == 3 && a.getComida().equalsIgnoreCase("Snack")){
+                snack.add(i,a);
+                i++;
+            }
+            model.addAttribute("snacktres", snack);
+        }
+
+        i= 0;
+        for (Alimento a : alimentos) {
+            if(a.getN_dieta() == 3 && a.getComida().equalsIgnoreCase("Cena")){
+                cena.add(i,a);
+                i++;
+            }
+            model.addAttribute("cenatres", cena);
+        }
+        return "dieta/dietatres";
+    }
+
+    @RequestMapping("/dieta/dietacuatro")
+    public String Dietacuatro(Model model){
+        List<Alimento> alimentos = alimentoService.getAll();
+        List<Alimento> desayuno= new ArrayList<>();
+        List<Alimento> almuerzo= new ArrayList<>();
+        List<Alimento> snack= new ArrayList<>();
+        List<Alimento> cena= new ArrayList<>();
+        int i= 0;
+        for (Alimento a : alimentos) {
+            if(a.getN_dieta() == 4 && a.getComida().equalsIgnoreCase("Desayuno")){
+                desayuno.add(i,a);
+                i++;
+            }
+            model.addAttribute("desayunocuatro", desayuno);
+        }
+
+        i= 0;
+        for (Alimento a : alimentos) {
+            if(a.getN_dieta() == 4 && a.getComida().equalsIgnoreCase("Almuerzo")){
+                almuerzo.add(i,a);
+                i++;
+            }
+            model.addAttribute("almuerzocuatro", almuerzo);
+        }
+
+        i= 0;
+        for (Alimento a : alimentos) {
+            if(a.getN_dieta() == 4 && a.getComida().equalsIgnoreCase("Snack")){
+                snack.add(i,a);
+                i++;
+            }
+            model.addAttribute("snackcuatro", snack);
+        }
+
+        i= 0;
+        for (Alimento a : alimentos) {
+            if(a.getN_dieta() == 4 && a.getComida().equalsIgnoreCase("Cena")){
+                cena.add(i,a);
+                i++;
+            }
+            model.addAttribute("cenacuatro", cena);
+        }
+        return "dieta/dietacuatro";
+    }
+
+    @RequestMapping("/dieta/dietacinco")
+    public String Dietacinco(Model model){
+        List<Alimento> alimentos = alimentoService.getAll();
+        List<Alimento> desayuno= new ArrayList<>();
+        List<Alimento> almuerzo= new ArrayList<>();
+        List<Alimento> snack= new ArrayList<>();
+        List<Alimento> cena= new ArrayList<>();
+        int i= 0;
+        for (Alimento a : alimentos) {
+            if(a.getN_dieta() == 5 && a.getComida().equalsIgnoreCase("Desayuno")){
+                desayuno.add(i,a);
+                i++;
+            }
+            model.addAttribute("desayunocinco", desayuno);
+        }
+
+        i= 0;
+        for (Alimento a : alimentos) {
+            if(a.getN_dieta() == 5 && a.getComida().equalsIgnoreCase("Almuerzo")){
+                almuerzo.add(i,a);
+                i++;
+            }
+            model.addAttribute("almuerzocinco", almuerzo);
+        }
+
+        i= 0;
+        for (Alimento a : alimentos) {
+            if(a.getN_dieta() == 5 && a.getComida().equalsIgnoreCase("Snack")){
+                snack.add(i,a);
+                i++;
+            }
+            model.addAttribute("snackcinco", snack);
+        }
+
+        i= 0;
+        for (Alimento a : alimentos) {
+            if(a.getN_dieta() == 5 && a.getComida().equalsIgnoreCase("Cena")){
+                cena.add(i,a);
+                i++;
+            }
+            model.addAttribute("cenacinco", cena);
+        }
+        return "dieta/dietacinco";
     }
 
    /* @RequestMapping("/dieta/verNombres")
